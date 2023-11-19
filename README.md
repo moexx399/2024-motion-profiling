06c4af072923193fb98b821ddc21cd43322af57b
moexx399 <brandonmoe.personal@gmail.com>
2023-11-19 15:25:24 -0800

    working sim

---
+++ README.md
@@ -0,0 +1,13 @@
+https://frcsdk.reduxrobotics.com/ReduxLib_2023.json
+
+
+https://github.com/Mechanical-Advantage/AdvantageScope
+
+
+
+src/mainjava/frc/robot/RobotContainer.java
+- Create sendable chooser
+- Add all robot commands
+
+src\main\java\frc\robot\subsystems\swervedrive\SwerveSubsystem.java
+- add AutoBuilder.configureHolonomic to init function
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import java.io.File;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very
+ * little robot logic should actually be handled in the {@link Robot} periodic
+ * methods (other than the scheduler calls).
+ * Instead, the structure of the robot (including subsystems, commands, and
+ * trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  // The robot's subsystems and commands are defined here...
+  private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+      "swerve"));
+  private final SendableChooser<Command> autoChooser;
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Add all actions to PathPlanner
+    NamedCommands.registerCommand("Action 1", new PrintCommand("Performing Action 1."));
+    NamedCommands.registerCommand("Action 2", new PrintCommand("Performing Action 2."));
+    NamedCommands.registerCommand("Action 3", new PrintCommand("Performing Action 3."));
+
+    // Make chooser on smart dashboard
+    autoChooser = AutoBuilder.buildAutoChooser("Do Nothing");
+    SmartDashboard.putData("Auto Chooser", autoChooser);
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return autoChooser.getSelected();
+  }
+
+  public void setDriveMode() {
+    // drivebase.setDefaultCommand();
+  }
+
+  public void setMotorBrake(boolean brake) {
+    drivebase.setMotorBrake(brake);
+  }
+}
